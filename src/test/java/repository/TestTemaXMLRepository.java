8cc8ef13b7b09eceec96f41e534e8cf6d388a256
Dicianu Ioan-Alexandru <dicianuioanalexandru@gmail.com>
2022-04-19 00:46:19 +0300

    added test cases for lab3 take home

--- src/test/java/repository/TestTemaXMLRepository.java
+++ src/test/java/repository/TestTemaXMLRepository.java
@@ -12,6 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class TestTemaXMLRepository {
@@ -49,6 +50,55 @@ public class TestTemaXMLRepository {
         final Tema assignment = new Tema("", "description", 1, 2);
         final Tema savedAssignment = assignmentXMLRepository.save(assignment);
         assertNull(savedAssignment);
+    }
+
+    @Test
+    public void save_returnsNull_whenAssignmentHasNullDescription_WBT() {
+        final Tema assignment = new Tema("1", null, 1, 2);
+        final Tema savedAssignment = assignmentXMLRepository.save(assignment);
+        assertNull(savedAssignment);
+    }
+
+    @Test
+    public void save_returnsNull_whenAssignmentHasEmptyDescription_WBT() {
+        final Tema assignment = new Tema("1", "", 1, 2);
+        final Tema savedAssignment = assignmentXMLRepository.save(assignment);
+        assertNull(savedAssignment);
+    }
+
+    @Test
+    public void save_returnsNull_whenAssignmentHasDeadlineLessThanLowerBound_WBT() {
+        final Tema assignment = new Tema("1", "description", 0, 2);
+        final Tema savedAssignment = assignmentXMLRepository.save(assignment);
+        assertNull(savedAssignment);
+    }
+
+    @Test
+    public void save_returnsNull_whenAssignmentHasDeadlineGreaterThanUpperBound_WBT() {
+        final Tema assignment = new Tema("1", "description", 15, 2);
+        final Tema savedAssignment = assignmentXMLRepository.save(assignment);
+        assertNull(savedAssignment);
+    }
+
+    @Test
+    public void save_returnsNull_whenAssignmentHasStartlineLessThanLowerBound_WBT() {
+        final Tema assignment = new Tema("1", "description", 1, 0);
+        final Tema savedAssignment = assignmentXMLRepository.save(assignment);
+        assertNull(savedAssignment);
+    }
+
+    @Test
+    public void save_returnsNull_whenAssignmentHasDeadlineBeforeStartline_WBT() {
+        final Tema assignment = new Tema("1", "description", 1, 2);
+        final Tema savedAssignment = assignmentXMLRepository.save(assignment);
+        assertNull(savedAssignment);
+    }
+
+    @Test
+    public void save_returnsSavedAssignment_whenAssignmentIsValid_WBT() {
+        final Tema assignment = new Tema("1", "description", 2, 1);
+        final Tema savedAssignment = assignmentXMLRepository.save(assignment);
+        assertEquals(assignment, savedAssignment);
     }
 
 }
